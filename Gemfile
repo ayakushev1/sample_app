39a2f3c118fd27cbe23643a4dda34da1aa3b6641
ayakushev <ayakushev>
2013-03-16 14:41:02 +0400

    User signup coplete

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'gravatar_image_tag', '1.0.0.pre2'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,11 +47,23 @@ describe UsersController do
     end
   end
 
-    describe "failure" do
+  describe "POST 'create'" do
 
+    describe "failure" do
+      
       before(:each) do
         @attr = { :name => "", :email => "", :password => "",
                   :password_confirmation => "" }
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
       end
 
       it "should not create a user" do
@@ -59,20 +71,7 @@ describe UsersController do
           post :create, :user => @attr
         end.should_not change(User, :count)
       end
-
-      it "should have the right title" do
-        post :create, :user => @attr
-        response.should have_selector("title", :content => "Sign up")
-      end
-
-      it "should render the 'new' page" do
-        post :create, :user => @attr
-        response.should render_template('new')
-      end
     end
-  end
-
-  describe "POST 'create'" do
 
     describe "success" do
 
@@ -86,7 +85,7 @@ describe UsersController do
           post :create, :user => @attr
         end.should change(User, :count).by(1)
       end
-
+      
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
@@ -96,7 +95,13 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
+
   end
 
 end
