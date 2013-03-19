b6cc83902f531dea5932b791e1ebde0af5f222b6
ayakushev <ayakushev>
2013-03-20 00:07:20 +0400

    llll

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SampleApp
 #    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     
     # Heroku requires this to be false
-    config.assets.initialize_on_precompile=false
+ #   config.assets.initialize_on_precompile=false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
