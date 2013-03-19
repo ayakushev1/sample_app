2f6cecccd225db32347049ae0fcf1626b5ab0e10
ayakushev <ayakushev>
2013-03-20 01:44:32 +0400

    jjj

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+config.assets.precompile += %w( custom.css )
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
