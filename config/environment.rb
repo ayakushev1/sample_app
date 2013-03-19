88bbc0204eb62dfd0cde70a7f90fb15fcfa63f2d
ayakushev <ayakushev>
2013-03-20 01:32:54 +0400

    jjj

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#config.assets.precompile += %w( custom.css )
+
 # Initialize the rails application
 SampleApp::Application.initialize!
 
-#config.assets.precompile += %w( custom.css )
